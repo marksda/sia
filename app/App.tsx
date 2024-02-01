720beba0fde669465d509b2bb46a76b7757cf489
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-02-01 13:32:36 +0700

    cso

--- app/App.tsx
+++ app/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Button, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from '../navigation/app.navigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AssetIconsPack } from './app-icons-pack';
-
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME</Text>
-  </Layout>
-);
 
 export const App = (): React.ReactElement => (
   <>
