a0637fd8f7881251955f75389f8291ce20ee5eab
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-02-04 09:40:57 +0700

    cso

--- app/App.tsx
+++ app/App.tsx
@@ -4,13 +4,17 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { AppNavigator } from '../navigation/app.navigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AssetIconsPack } from './app-icons-pack';
-import { default as theme } from '../theme.json';
+import { default as theme } from './theme.json';
 import { StatusBar } from '../components/status-bar.component';
+import { default as mapping } from './mapping.json';
 
 export const App = (): React.ReactElement => (
   <>
     <IconRegistry icons={[AssetIconsPack]} />
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+    <ApplicationProvider 
+      {...eva} 
+      customMapping={mapping}
+      theme={{ ...eva.light, ...theme }}>
       <SafeAreaProvider>
         <StatusBar />
         <AppNavigator />
