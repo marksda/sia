b379b8e19d3fcd3c28df848bdb1c6dd50627050f
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-02-29 16:43:36 +0700

    cso

--- features/entities/token.ts
+++ features/entities/token.ts
@@ -1,12 +1,4 @@
 export interface IToken {
-    // userId: string|null;
-    // userName: string|null;
-    // userEmail: string|null;    
-    // hakAkses: string|null,
-    // accessToken: string|null;
-    // refreshToken: string|null;
-    // expireIn: string|null;
-    // sessionId: string|null;
     id: string|null;
     nama: string|null;
     token: string|null;
