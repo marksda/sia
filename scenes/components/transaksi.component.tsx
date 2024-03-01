be2bdab13311412ad5ad7c04793e9c3ebc354dee
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-03-01 16:57:59 +0700

    cso

--- scenes/components/transaksi.component.tsx
+++ scenes/components/transaksi.component.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useState } from 'react';
-import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { ListRenderItemInfo, SafeAreaView, StyleSheet } from 'react-native';
+import { Button, Layout, List, ListItem } from '@ui-kitten/components';
 import { useGetDaftarBarangQuery } from '../../services/api-rtkquery-service';
 import { IQueryParamFilters } from '../../features/entities/query-param-filters';
+import { IBarang } from '../../features/entities/barang';
 
 
 interface ITransaksiScreenProps {
@@ -11,25 +12,37 @@ interface ITransaksiScreenProps {
 };
 
 export const TransaksiScreen: FC<ITransaksiScreenProps> = ({initSelectedFilters, navigation}) => {
-
+  
   const [currentPage, setCurrentPage] = useState<number>(initSelectedFilters?.pageNumber!);
   const [pageSize, setPageSize] = useState<number>(initSelectedFilters?.pageSize!);
   const [queryParams, setQueryParams] = useState<IQueryParamFilters>({
     ...initSelectedFilters!, pageNumber: currentPage, pageSize
   });
   const { data: daftarBarang, isLoading: isLoadingFetchDaftarBarang } = useGetDaftarBarangQuery(queryParams);  
+  
 
   const navigateDetails = () => {
     navigation.navigate('Laporan');
   };
 
+  const renderItem = ({item}: ListRenderItemInfo<IBarang>): React.ReactElement => (
+    <ListItem title={`${item.nama}`} />
+  );
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <TopNavigation title='Transaksi' alignment='center'/>
-      <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>        
+        <List style={styles.container}
+          data={daftarBarang}
+          renderItem={renderItem} />    
         <Button onPress={navigateDetails}>OPEN Laporan</Button>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    maxHeight: 180,
+  },
+});
