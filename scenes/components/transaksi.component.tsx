19a512761902ab1dc639c6199aa0f4fc52fd2ccd
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-03-02 16:12:11 +0700

    cso

--- scenes/components/transaksi.component.tsx
+++ scenes/components/transaksi.component.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
-import { ListRenderItemInfo, SafeAreaView, StyleSheet } from 'react-native';
-import { Button, Layout, List, ListItem } from '@ui-kitten/components';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Button, Card, Layout, Text } from '@ui-kitten/components';
 import { useGetDaftarBarangQuery } from '../../services/api-rtkquery-service';
 import { IQueryParamFilters } from '../../features/entities/query-param-filters';
 import { IBarang } from '../../features/entities/barang';
@@ -28,16 +28,43 @@ export const TransaksiScreen: FC<ITransaksiScreenProps> = ({initSelectedFilters,
     navigation.navigate('Laporan');
   };
 
-  const renderItem = ({item}: ListRenderItemInfo<IBarang>): React.ReactElement => {
-    return <ListItem title={`${item.id} - ${item.nama}`} />;
-  };
+  // const renderItem = ({item}: ListRenderItemInfo<IBarang>): React.ReactElement => {
+  //   return <ListItem title={`${item.id} - ${item.nama}`} />;
+  // };
+
+  const Header = (item: IBarang): React.ReactElement => (
+    <View>
+      <Text category='h6'>
+        {`${item.id}`}
+      </Text>
+      <Text category='s1'>
+        {`${item.nama}`}
+      </Text>
+    </View>
+  );
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <List style={styles.container}
+      {/* <List style={styles.container}
           data={daftarBarang}
-          renderItem={renderItem} />    
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>   
+          renderItem={renderItem} />     */}
+      <Layout style={styles.containerTop} level='1'>   
+        { daftarBarang ? (
+            daftarBarang.map((item) => (
+              <Card
+                key={item.id as string}
+                style={styles.card}
+                header={Header(item)}
+              >
+                <Text>
+                  With Header
+                </Text>
+              </Card>
+            ))            
+          ) : null
+        }        
+      </Layout>
+      <Layout style={styles.containerBottom}>
         <Button onPress={navigateDetails}>OPEN Laporan</Button>
       </Layout>
     </SafeAreaView>
@@ -45,7 +72,16 @@ export const TransaksiScreen: FC<ITransaksiScreenProps> = ({initSelectedFilters,
 };
 
 const styles = StyleSheet.create({
-  container: {
-    maxHeight: 180,
+  containerTop: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 8,
   },
+  card: {
+    flex: 1,
+    margin: 2,
+  },
+  containerBottom: {
+    paddingHorizontal: 8
+  }
 });
