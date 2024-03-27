d4fecb0a6f0deedf3ff8d58e1e8f4b39d08a2417
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-03-27 09:30:55 +0700

    cso

--- scenes/components/transaksi.component.tsx
+++ scenes/components/transaksi.component.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
-import { Button, Card, Divider, Layout, List, Text } from '@ui-kitten/components';
+import { Alert, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
+import { Button, Card, Divider, Icon, List, Text } from '@ui-kitten/components';
 import { useGetDaftarBarangQuery } from '../../services/api-rtkquery-service';
 import { IQueryParamFilters } from '../../features/entities/query-param-filters';
 import { IBarang } from '../../features/entities/barang';
 import { ITransaki } from '../../features/entities/transaksi';
 import * as _ from "lodash";
 import { IItemTransaki } from '../../features/entities/item-transaksi';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 interface ITransaksiScreenProps {
   initSelectedFilters?: IQueryParamFilters;
@@ -83,7 +84,7 @@ export const TransaksiScreen: FC<ITransaksiScreenProps> = ({initSelectedFilters,
         }
 
         return newTransaksi;
-      })
+      });
     }
   };
 
@@ -104,22 +105,100 @@ export const TransaksiScreen: FC<ITransaksiScreenProps> = ({initSelectedFilters,
 
       </View>
       <View style={styles.contentItemDetail}>
-        <View style={styles.itemDetailTitle}>
+        <View style={styles.itemDetailTop}>
           <Text style={styles.title}>{`${item.item?.id} - ${item.item?.nama!}`}</Text>
+          <TouchableHighlight onPress={() => _onPressIconDelete(item.item?.id!)} underlayColor="#FCD4F2">
+            <Icon
+              style={styles.iconDelete}
+              name='trash-2-outline'              
+            />
+          </TouchableHighlight>          
         </View>
-        <View style={styles.itemDetailCount}>
+        <View style={styles.itemDetailBottom}>
           <Text style={styles.titleHarga}>{`Rp. ${item.harga}`}</Text>
-          <TextInput
-            style={styles.input}
-            caretHidden={true}
-            onChangeText={onChangeNumber}
-            value={number}
-            keyboardType="numeric"
-          />
+          <View style={styles.itemDetailCount}>
+            <TouchableHighlight onPress={() => _onPressIconPlus(item.item?.id!)} underlayColor="#FCD4F2">
+              <Icon
+                style={styles.icon}
+                name='plus-outline'
+              />
+            </TouchableHighlight>
+            <TextInput
+              style={styles.input}
+              caretHidden={false}
+              onChangeText={onChangeNumber}
+              value={item.jumlah.toString()}
+              keyboardType="numeric"
+            />
+            <TouchableHighlight onPress={() => _onPressIconMinus(item.item?.id!)} underlayColor="#FCD4F2">
+              <Icon
+                style={styles.icon}
+                name='minus-outline'
+              />
+            </TouchableHighlight>
+          </View>
         </View>        
       </View>
     </View>    
   );
+
+  const _onPressIconDelete = (id: string) => {
+    if(transaksi != null) {  
+      setTransaksi((prev) => {
+        let newTransaksi = _.cloneDeep(prev);
+        _.remove(newTransaksi!.daftarItemTransaksi, (currentObject) => {
+          return currentObject.item?.id === id;
+        });
+
+        return newTransaksi;
+      })
+    }
+  };
+
+  const _onPressIconMinus = (id: string) => {
+    if(transaksi != null) {  
+      setTransaksi((prev) => {
+        let newTransaksi = _.cloneDeep(prev);
+        let existItem = _.find(newTransaksi!.daftarItemTransaksi, (currentObject) => (currentObject.item?.id === id));
+        if(existItem != undefined) {
+          if(existItem.jumlah == 1) {
+            _.remove(newTransaksi!.daftarItemTransaksi, (currentObject) => {
+              return currentObject.item?.id === id;
+            });
+          }
+          else {
+            let indexItem = _.findIndex(newTransaksi!.daftarItemTransaksi, (currentObject) => (currentObject.item?.id === id));
+
+            existItem.jumlah -= 1;
+            existItem.total = existItem.jumlah * existItem.harga;
+
+            newTransaksi?.daftarItemTransaksi.splice(indexItem, 1, existItem);
+          }          
+        }
+
+        return newTransaksi;
+      })
+    }
+  };
+
+  const _onPressIconPlus = (id: string) => {
+    if(transaksi != null) {  
+      setTransaksi((prev) => {
+        let newTransaksi = _.cloneDeep(prev);
+        let existItem = _.find(newTransaksi!.daftarItemTransaksi, (currentObject) => (currentObject.item?.id === id));
+        if(existItem != undefined) {
+          let indexItem = _.findIndex(newTransaksi!.daftarItemTransaksi, (currentObject) => (currentObject.item?.id === id));
+
+          existItem.jumlah += 1;
+          existItem.total = existItem.jumlah * existItem.harga;
+
+          newTransaksi?.daftarItemTransaksi.splice(indexItem, 1, existItem);
+        }
+
+        return newTransaksi;
+      })
+    }
+  };
 
   return (
     <SafeAreaView style={styles.mainContainer}>
@@ -159,6 +238,7 @@ export const TransaksiScreen: FC<ITransaksiScreenProps> = ({initSelectedFilters,
       </View>
     </SafeAreaView>
   );
+
 };
 
 const styles = StyleSheet.create({
@@ -208,17 +288,32 @@ const styles = StyleSheet.create({
     flex: 4,
     flexDirection: 'column',
   },
-  itemDetailTitle: {    
+  itemDetailTop: {   
+    flex: 1, 
+    flexDirection: 'row', 
     backgroundColor: '#f9c2ff',
-    padding: 8,
+    paddingVertical: 8,
+    paddingLeft: 8,
+    paddingRight: 36,
+    justifyContent: 'space-between',
     // marginVertical: 8,
     // marginHorizontal: 16,
   },
-  itemDetailCount: {   
+  itemDetailBottom: {   
     flex: 1,
     flexDirection: 'row', 
     backgroundColor: '#f9c2ff',
     padding: 8,
+    justifyContent: 'space-between',
+  },
+  itemDetailCount: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderRadius: 25,
+    borderColor: 'grey',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    alignItems: 'center',
   },
   title: {
     fontSize: 16,
@@ -228,12 +323,22 @@ const styles = StyleSheet.create({
   },  
   input: {
     height: 20,
-    width: 40,
+    width: 30,
     // borderWidth: 1,
     fontSize: 14,
     textAlignVertical: 'top',
     textAlign: 'center',
     paddingVertical: 0,
     paddingHorizontal: 2,
+  },
+  icon: {
+    width: 18,
+    height: 18,
+    color: 'grey',
+  },
+  iconDelete: {
+    width: 22,
+    height: 22,
+    color: 'grey',
   },
 });
