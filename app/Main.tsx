b4cf1bd6fed0d4fac2b9e26aada144f9b334991d
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-15 15:02:35 +0700

    cso

--- app/Main.tsx
+++ app/Main.tsx
@@ -4,26 +4,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { SignInScreen } from "../scenes/auth/sign-in.component";
 import { StatusBar } from "../components/status-bar.component";
 import { AppNavigator } from "../navigation/app.navigator";
-import { Dimensions,  } from "react-native";
+import { useWindowDimensions,  } from "react-native";
 import { Text } from "@ui-kitten/components";
 
-const windowDimensions = Dimensions.get('window');
-
 export const Main = (): React.ReactElement => {
+    const dimensions = useWindowDimensions();
     const token = useAppSelector(state => state.persisted.token); 
-    const [dimensions, setDimensions] = useState({
-        window: windowDimensions
-      });
-
-    useEffect(() => {
-        const subscription = Dimensions.addEventListener(
-          'change',
-          ({window}) => {
-            setDimensions({window});
-          },
-        );
-        return () => subscription?.remove();
-    });
 
     return (
         <SafeAreaProvider>
@@ -31,10 +17,10 @@ export const Main = (): React.ReactElement => {
                 token == null ? 
                 <SignInScreen /> : 
                 <>
-                    <StatusBar hidden={false} backgroundColor="#61dafb"/>
+                    <StatusBar hidden={true} backgroundColor="#61dafb"/>
                     <AppNavigator />
                     <Text>
-                        scale: {dimensions.window.scale} - width: {dimensions.window.width}
+                        scale: {dimensions.scale} - width: {dimensions.width}
                     </Text>
                 </>                
             }
--- navigation/home.navigator.tsx
+++ navigation/home.navigator.tsx
@@ -2,13 +2,19 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { HomeDrawer } from "../scenes/home/home-drawer.component";
 import { TransaksiScreen } from "../scenes/components/transaksi.component";
 import { LaporanScreen } from "../scenes/components/laporan.component";
+import { useWindowDimensions } from "react-native";
 
 
 const Drawer = createDrawerNavigator();
 
 export const HomeNavigator = (): React.ReactElement => {
+    const dimensions = useWindowDimensions();
+
     return (
         <Drawer.Navigator
+            screenOptions={{
+                drawerType: dimensions.width >= 768 ? 'permanent' : 'front',
+            }}   
             drawerContent={props => <HomeDrawer {...props} />}>
             <Drawer.Screen name='Transaksi'>
                 {
