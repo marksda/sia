71f5cd71dbd4d3c30685fee903e12f6f9a7a5035
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-08-07 09:02:54 +0700

    cso

--- layouts/akutansi-app/formulir-scan-printer.tsx
+++ layouts/akutansi-app/formulir-scan-printer.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@ui-kitten/components";
+import { Button, Text } from "@ui-kitten/components";
 import { FC } from "react";
 import { usePrintersDiscovery } from "react-native-esc-pos-printer";
 
@@ -6,7 +6,11 @@ const FormulirScanPrinterLayout: FC = () => {
     const {start, printerError, isDiscovering, printers} = usePrintersDiscovery();
 
     return (
-        <Text>Scan printer</Text>
+        <>
+            <Button>
+                
+            </Button>
+        </>
     );
 };
 
