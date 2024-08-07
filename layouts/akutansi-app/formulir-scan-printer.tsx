dde4501654af071afd1b2c3362dde05dcf50b2a9
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-08-07 09:52:20 +0700

    cso

--- layouts/akutansi-app/formulir-scan-printer.tsx
+++ layouts/akutansi-app/formulir-scan-printer.tsx
@@ -1,17 +1,41 @@
-import { Button, Text } from "@ui-kitten/components";
+import { Button, Spinner, Text } from "@ui-kitten/components";
 import { FC } from "react";
+import { ImageProps, StyleSheet, View } from "react-native";
 import { usePrintersDiscovery } from "react-native-esc-pos-printer";
 
 const FormulirScanPrinterLayout: FC = () => {
     const {start, printerError, isDiscovering, printers} = usePrintersDiscovery();
 
+    const LoadingIndicator = (props: Partial<ImageProps> | undefined): React.ReactElement => (
+        <View style={[props!.style, styles.indicator]}>
+          <Spinner size='small' />
+        </View>
+      );
     return (
         <>
-            <Button>
-                
+            <Button
+                style={styles.button}
+                appearance='outline'
+                accessoryLeft={LoadingIndicator}
+            >
+                Loading
             </Button>
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    button: {
+      margin: 2,
+    },
+    indicator: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
 
 export default FormulirScanPrinterLayout;
